67185ecd59e962e82e81b41f32dc403a63b8e28c
SANKET MAJITHIYA <143784345+Sanketmajithiya@users.noreply.github.com>
2025-08-21 12:59:45 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 from EShop.wsgi import application
 
 app = application
-=======
-from EShop.EShop.wsgi import application 
 
-app = application
->>>>>>> 54c6a92 (Added changes and fixed file names)
