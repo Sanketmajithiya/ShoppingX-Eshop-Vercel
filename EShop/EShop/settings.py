f93667d4d4e30732b201c5e3a5f384114a7503da
SANKET MAJITHIYA <143784345+Sanketmajithiya@users.noreply.github.com>
2025-08-21 15:34:10 +0530

    Update settings.py

--- EShop/EShop/settings.py
+++ EShop/EShop/settings.py
@@ -155,7 +155,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Store/static/app')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'Store/static'),
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
